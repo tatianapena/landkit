d46a7a4ef60480673fcaadcec3bb177040ff7353
Tatiana Pena <tatiana.pena@koombea.com>
2021-04-02 23:38:56 -0500

    basic card implementation for article

--- js/index.js
+++ js/index.js
@@ -2,6 +2,65 @@ const stickyHeader = document.querySelector(".js-sticky")
 const result = document.querySelector(".js-totalResults")
 const searchInput = document.querySelector(".js-search")
 const articles = document.querySelector(".articles")
+// let bodyElement = document.body;
+// let cardElement = document.createElement('section');
+// let containerElement = document.createElement('div');
+// let cardContainer = document.createElement('div');
+// let cardHeader = document.createElement('div');
+// let cardFavorite = document.createElement('div');
+// let cardStar = document.createElement('img');
+// let cardPrice = document.createElement('div');
+// let cardValue = document.createElement('p');
+// let cardImage = document.createElement('div');
+// let cardPhoto = document.createElement('img');
+// let cardTitle = document.createElement('h5');
+// let cardParagraph = document.createElement('p');
+// let cardFooter = document.createElement('div');
+// let cardSign = document.createElement('div');
+// let cardAvatar = document.createElement('img');
+// let cardName = document.createElement('p');
+// let cardDate = document.createElement('time');
+// let cardBtn =document.createElement('div');
+// let cardLink = document.createElement('a');
+// let cardArrow = document.createElement('img');
+
+
+// cardElement.className ='card';
+// containerElement.className ='container';
+// cardContainer.className ='card__container';
+// cardHeader.className ='card__header';
+// cardFavorite.className ='card__favorite';
+// cardStar.src ='./assets/Star-fill.png';
+// cardStar.setAttribute("alt","fill star");
+// cardPrice.className ='card__price';
+// cardValue.className ='card__value';
+// cardValue.innerText ='$49/mo';
+// cardImage.className ='card__image';
+// cardPhoto.src ='https://picsum.photos/640/426';
+// cardPhoto.setAttribute("alt", "random image");
+// cardTitle.className ='card__title';
+// cardTitle.innerText ='Keeping the dream alive by traveling the world.';
+// cardParagraph.className ='card__paragraph';
+// cardParagraph.innerText = 'Integrate the latest technologies with an innovative platform...';
+// cardFooter.className ='card__footer';
+// cardSign.className ='card__sign';
+// cardAvatar.src ='https://randomuser.me/';
+// cardAvatar.setAttribute("alt", "random avatar image"),
+// cardName.className ='card__name';
+// cardName.innerText ='ALIVE COOPER';
+// cardDate.className ='card__date';
+// cardDate.innerText ='MAY 02';
+// cardBtn.className ='card__btn';
+// cardLink.className ='card__link';
+// cardLink.setAttribute("href", "#");
+// cardLink.innerText = 'Load more';
+// cardArrow.src ="./assets/arrow.png";
+
+
+// bodyElement.appendChild(cardElement);
+// cardElement.append(cardImage, cardPhoto);
+
+
 
 document.addEventListener("DOMContentLoaded", async function() {
   const articlesData = await fetchData();
@@ -70,7 +129,7 @@ function stickyController() {
   }
 }
 
-// function for results 
+
 
 
 
